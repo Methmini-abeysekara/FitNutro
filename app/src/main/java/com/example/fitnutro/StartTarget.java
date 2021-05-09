a0ec64cd5d5685e460479ceb06b2b04be29e80ed
IsuruPriyal <isurupriyalnc@gmail.com>
2021-05-09 16:00:33 +0530

    Update Task Class

---
+++ app/src/main/java/com/example/fitnutro/StartTarget.java
@@ -0,0 +1,28 @@
+package com.example.fitnutro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartTarget extends AppCompatActivity {
+
+    Button Gain_button,Loss_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_target);
+
+        Gain_button = findViewById(R.id.W_gain_button);
+        Gain_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(StartTarget.this,G_ScheduleList.class);
+                startActivity(i);
+            }
+        });
+    }
+}
