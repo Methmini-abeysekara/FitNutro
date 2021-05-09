7407af80d25774f7fadd42d334731611ecbea019
Minuli-Lavanya <minulilavanya@gmail.com>
2021-05-09 08:30:43 +0530

    Add image to update page and add navigate button from welcometimetable page to welcome page

--- app/src/main/java/com/example/fitnutro/WelcomeToTimetablePage.java
+++ app/src/main/java/com/example/fitnutro/WelcomeToTimetablePage.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class WelcomeToTimetablePage extends AppCompatActivity {
     private Button button1, button2;
+    private ImageButton imageButton6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,14 @@ public class WelcomeToTimetablePage extends AppCompatActivity {
                 openwaterintakecalculator();
             }
         });
+
+        imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+        imageButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openwelcomepage();
+            }
+        });
     }
 
     public void opentimetable(){
@@ -37,6 +47,10 @@ public class WelcomeToTimetablePage extends AppCompatActivity {
     }
     public void openwaterintakecalculator(){
         Intent intent = new Intent (this, WaterIntakeCalculator.class);
+        startActivity(intent);
+    }
+    public void openwelcomepage(){
+        Intent intent = new Intent (this, WelcomePage.class);
         startActivity(intent);
     }
 }
