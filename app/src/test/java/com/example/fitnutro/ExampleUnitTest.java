0b2e3ef1e9664d7e3f1a365c5278fc80673abe6e
Methmini-abeysekara <manojaa415@gmail.com>
2021-05-09 16:40:27 +0530

    Add Testcase

--- app/src/test/java/com/example/fitnutro/ExampleUnitTest.java
+++ app/src/test/java/com/example/fitnutro/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.fitnutro;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,24 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    private Protein_Calculator protein_Calculator;
+
+    @Before
+    public void setup() {
+        protein_Calculator = new Protein_Calculator();
+    }
+
+    @Test
+    public void testProtein_Calculation() {
+        float result =  protein_Calculator.calProtein(80,1);
+        assertEquals(80.0, result,0.000001);
+    }
+
+
+
+
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
